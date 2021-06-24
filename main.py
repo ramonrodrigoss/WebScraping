254298881aee6fba06264b2c795014a48516445f
Ramon Rodrigo <ramonrodrigoss@gmail.com>
2021-06-24 23:55:02 +0000

    Pegando a informacao da pesquisa e mostrando na tela

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 
-#from selenium.webdriver.chrome.options import Options
-#chrome_options = Options()
-#chrome_options.headless = True
-#nav = webdriver.Chrome(options=chrome_options)
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.headless = True
+navegador = webdriver.Chrome(options=chrome_options)
 
 
 #Abrir Navegador
-navegador = webdriver.Chrome()
+#navegador = webdriver.Chrome()
 #Abrir o site do Google
 navegador.get("https://www.google.com/")
 #Pesquisando "Cotacao do dolar" 
 navegador.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("cotação dolar")
+#Apertando Enter para buscar
+navegador.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys(Keys.ENTER)
+
+#Pegando a informacao da cotacao do dolar do dia
+cotacao_dolar = navegador.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
+
+print(cotacao_dolar)
