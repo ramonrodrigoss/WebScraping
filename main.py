82b0b59cc1a693d07c7a38c511020896acb00536
Ramon Rodrigo <ramonrodrigoss@gmail.com>
2021-06-24 23:33:28 +0000

    Colocado codigo para executar navegar em background

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 
-
+#from selenium.webdriver.chrome.options import Options
+#chrome_options = Options()
+#chrome_options.headless = True
+#nav = webdriver.Chrome(options=chrome_options)
 
 driver = webdriver.Chrome()
+
+navegador= get.
